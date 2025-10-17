3277dd97949a2e65db84c892fb0155c144ffd01d
LepharamRamchiary <lepharamchiary@gmail.com>
2025-10-17 12:01:14 +0530

    added add user api

---
+++ server/src/models/user.model.js
@@ -0,0 +1,10 @@
+import mongoose, { Schema} from "mongoose";
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    }
+})
+
+export const User = mongoose.model("User", userSchema)
